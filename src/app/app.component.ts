066684e770cb358e802dce9fe6c60177267f18c1
TIMSLAPTOP\tijon <tijong@gmail.com>
2025-03-15 14:16:18 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,56 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Websitos';
+  ipsumList = ['ipsum', 'ipso', 'ispi'];
+  indexInit = [0];
+
+  hoverTextList: HoverText[] = [
+    {
+      index: 0,
+      list: ['ipsum', 'ipso', 'iskjfhxdiashzfkahfoihpi'],
+      style: '',
+    },
+    {
+      index: 0,
+      list: [
+        'dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat',
+      ],
+      style: '',
+    },
+    {
+      index: 0,
+      list: ['nulla', 'eene', 'tweee', 'drieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'],
+      style: '',
+    },
+  ];
+
+  //   constructor() {
+
+  //     setInterval(() => {
+  //       this.hoverTextList.push({
+  // index: 0,
+  // list: ["kjsanakkjasd"]
+
+  //       })
+  //     }, 1000);
+  //   }
+
+  rouletteOnHover(index: number) {
+    let length = this.hoverTextList[index].list.length;
+    if (length > 1) {
+      this.hoverTextList[index].index =
+        (this.hoverTextList[index].index + 1) % length;
+      this.hoverTextList[index].style = 'red';
+//     let temp = Math.floor(Math.random() * 6) 
+    }
+  }
+}
+
+export interface HoverText {
+  list: string[];
+  index: number;
+  style: string;
 }
